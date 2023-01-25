21612756dd9512241435512660cc941082e09e7a
Zane Davidson <zanerdavidson@gmail.com>
2023-01-24 20:48:56 -0800

    Update Cost.cs

--- Cost/Cost.cs
+++ Cost/Cost.cs
@@ -1,1 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
+public class Cost
+{
+    public double applyCost(double[] expectedOutputs, double[] predictedOutputs)
+    {
+        double squareCost = 0;
+        for(int i = 0; i < predictedOutputs.Length; i++)
+        {
+            double cost = predictedOutputs[i] - expectedOutputs[i];
+            squareCost = cost * cost;
+        }
+        return 0.5 * squareCost;
+    }
+
+
+    public double costDerivative(double expectedOutput, double predictedOutput)
+    {
+        return predictedOutput - expectedOutput;
+    }
+}
